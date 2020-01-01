94e4649811ddd843f96fd7bc12487dce0f5c1ff9
IzumiSy <beetle-noise@gmx.com>
2020-01-01 21:01:41 +0900

    Added test for remove function

--- tests/Tests/ConsistentHashing.elm
+++ tests/Tests/ConsistentHashing.elm
@@ -40,4 +40,44 @@ suite =
                         )
                     |> Expect.equalLists
                         (List.map Tuple.second sourceAndResults)
+        , test "remove" <|
+            \_ ->
+                let
+                    node1 =
+                        Node.new "node1"
+
+                    node2 =
+                        Node.new "node2"
+
+                    node3 =
+                        Node.new "node3"
+
+                    node4 =
+                        Node.new "node4"
+
+                    ch =
+                        ConsistentHashing.new Replica.default node1
+                            |> ConsistentHashing.add node2
+                            |> ConsistentHashing.add node3
+                            |> ConsistentHashing.add node4
+                            |> ConsistentHashing.remove node1
+                            |> ConsistentHashing.remove node3
+                            |> ConsistentHashing.remove node2
+                in
+                sourceAndResults
+                    |> List.map Tuple.first
+                    |> List.map
+                        (\value ->
+                            ch
+                                |> ConsistentHashing.getNode (Key.new value)
+                                |> Node.toString
+                        )
+                    |> Expect.equalLists
+                        [ "node4"
+                        , "node4"
+                        , "node4"
+                        , "node4"
+                        , "node4"
+                        , "node4"
+                        ]
         ]
