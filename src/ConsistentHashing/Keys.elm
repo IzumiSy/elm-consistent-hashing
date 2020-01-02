8c266fa353ecf7a592e41bc9cb29b217f3b447c9
IzumiSy <beetle-noise@gmx.com>
2020-01-02 23:56:47 +0900

    Use binary search for find function

--- src/ConsistentHashing/Keys.elm
+++ src/ConsistentHashing/Keys.elm
@@ -7,47 +7,74 @@ module ConsistentHashing.Keys exposing
     , remove
     )
 
+import Array
 import ConsistentHashing.Key as Key
 import ConsistentHashing.Node as Node
-import List.Extra as ExList
 
 
+{-| The reason why Keys has Array as an internal data structure is, `find` method of this module internally uses Binary Search algorithm.
+It requires the collection to have performance in accessing by its index, where exactly Array in Elm offers.
+-}
 type Keys
-    = Keys (List ( Node.Node, Key.Key ))
+    = Keys (Array.Array ( Node.Node, Key.Key ))
 
 
 empty : Keys
 empty =
-    Keys []
+    Keys Array.empty
 
 
+{-| Internal data structure of Keys is Array, but as an interface, List might be more generic, handy to use.
+Plus, `append` is less likely to be called by users, so here probably won't be any performance issue.
+-}
 append : List ( Node.Node, Key.Key ) -> Keys -> Keys
 append keys (Keys currentKeys) =
     keys
-        |> (++) currentKeys
+        |> (++) (Array.toList currentKeys)
         |> List.sortBy (Key.toString << Tuple.second)
+        |> Array.fromList
         |> Keys
 
 
 remove : Node.Node -> Keys -> Keys
 remove targetNode (Keys keys) =
-    Keys <| ExList.filterNot (\( node, _ ) -> Node.toString node == Node.toString targetNode) keys
+    Keys <| Array.filter (\( node, _ ) -> not (Node.toString node == Node.toString targetNode)) keys
 
 
+{-| Looks up all node keys by Binary Search
+-}
 find : Key.Key -> Keys -> Maybe Node.Node
-find targetKey (Keys keys) =
-    keys
-        |> ExList.find (\( _, key ) -> isAssignable key targetKey)
-        |> Maybe.map Tuple.first
+find targetKey ((Keys keys_) as keys) =
+    findInternal 0 (Array.length keys_) Nothing targetKey keys
 
 
 head : Keys -> Maybe Node.Node
 head (Keys keys) =
     keys
-        |> List.head
+        |> Array.get 0
         |> Maybe.map Tuple.first
 
 
-isAssignable : Key.Key -> Key.Key -> Bool
-isAssignable a b =
-    Key.toString a >= Key.toString b
+{-| Looks up the nearest key by recursive Binary Search
+This is way better in performance using linear search with List in case of many keys.
+-}
+findInternal : Int -> Int -> Maybe Node.Node -> Key.Key -> Keys -> Maybe Node.Node
+findInternal beginIndex endIndex currentNode key (Keys keys) =
+    if beginIndex > endIndex then
+        currentNode
+
+    else
+        let
+            median =
+                beginIndex + (endIndex - beginIndex) // 2
+        in
+        keys
+            |> Array.get median
+            |> Maybe.andThen
+                (\( medianNode, medianKey ) ->
+                    if Key.toString medianKey < Key.toString key then
+                        findInternal (median + 1) endIndex (Just medianNode) key (Keys keys)
+
+                    else
+                        findInternal beginIndex (median - 1) (Just medianNode) key (Keys keys)
+                )
